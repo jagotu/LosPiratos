bc910cc39b7cff0bb38c0d52da90edf454c6419e
tt.teichmann@gmail.com <tt.teichmann@gmail.com>
2018-01-15 11:37:36 +0100

    viewmodel, model: new game mock

--- src/com/vztekoverflow/lospiratos/viewmodel/Game.java
+++ src/com/vztekoverflow/lospiratos/viewmodel/Game.java
@@ -2,6 +2,13 @@ package com.vztekoverflow.lospiratos.viewmodel;
 
 import com.vztekoverflow.lospiratos.util.FxUtils;
 import com.vztekoverflow.lospiratos.util.Warnings;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.ShipEnhancement;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.ShipType;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.enhancements.*;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.ships.Brig;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.ships.Frigate;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.ships.Galleon;
+import com.vztekoverflow.lospiratos.viewmodel.shipEntitites.ships.Schooner;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.MapProperty;
 import javafx.beans.property.SimpleListProperty;
@@ -19,7 +26,7 @@ public class Game {
 
     private com.vztekoverflow.lospiratos.model.Game gameModel;
 
-    public Game(){
+    public Game() {
         this(new com.vztekoverflow.lospiratos.model.Game());
     }
 
@@ -40,9 +47,10 @@ public class Game {
             }
         });
     }
-    private void addNewTeamFromModel(com.vztekoverflow.lospiratos.model.Team teamModel){
-        if(teams.stream().anyMatch(p -> p.getName().equalsIgnoreCase(teamModel.getName()))){
-            Warnings.makeWarning(toString(),"Attempt to create a team with a name that is already used: " + teamModel.getName());
+
+    private void addNewTeamFromModel(com.vztekoverflow.lospiratos.model.Team teamModel) {
+        if (teams.stream().anyMatch(p -> p.getName().equalsIgnoreCase(teamModel.getName()))) {
+            Warnings.makeWarning(toString(), "Attempt to create a team with a name that is already used: " + teamModel.getName());
             return;
         }
         Team t = new Team(this, teamModel);
@@ -52,13 +60,13 @@ public class Game {
     /*
      * @returns null if team with this name (case insensitive) already exists
      */
-    public Team createAndAddNewTeam(String teamName, Color teamColor){
-        if(teamName == null || teamName.isEmpty()){
+    public Team createAndAddNewTeam(String teamName, Color teamColor) {
+        if (teamName == null || teamName.isEmpty()) {
             Warnings.makeWarning(toString(), "Attempt to create a team with null or empty name.");
             return null;
         }
-        if(teams.stream().anyMatch(p -> p.getName().equalsIgnoreCase(teamName))){
-            Warnings.makeWarning(toString(),"Attempt to create a team with a name that is already used: " + teamName);
+        if (teams.stream().anyMatch(p -> p.getName().equalsIgnoreCase(teamName))) {
+            Warnings.makeWarning(toString(), "Attempt to create a team with a name that is already used: " + teamName);
             return null;
         }
         com.vztekoverflow.lospiratos.model.Team teamModel = new com.vztekoverflow.lospiratos.model.Team();
@@ -68,51 +76,57 @@ public class Game {
         //at this place, gameModel.teamsProperty's change calls my observer
         //   which then adds the team to this game's collection (if valid)
         Team t = findTeamByName(teamName);
-        if(t == null) return null; //this means the model was somehow invalid
+        if (t == null) return null; //this means the model was somehow invalid
         t.initialize();
         return t;
     }
+
     public com.vztekoverflow.lospiratos.model.Game getGameModel() {
         return gameModel;
     }
 
 
-
     private ListProperty<Team> teams = new SimpleListProperty<>(FXCollections.observableArrayList());
+
     public ObservableList<Team> getTeams() {
         return teams.get();
     }
+
     public ListProperty<Team> teamsProperty() {
         return teams;
     }
 
     private MapProperty<String, Ship> allShips = new SimpleMapProperty<>(FXCollections.observableHashMap());
+
     public Collection<Ship> getAllShips() {
         return allShips.get().values();
     }
+
     public MapProperty<String, Ship> allShipsProperty() {
         return allShips;
     }
 
-    public boolean mayCreateShipWithName(String name){
+    public boolean mayCreateShipWithName(String name) {
         if (name == null || name.isEmpty()) {
             Warnings.makeDebugWarning(toString(), "Ship's name is empty or null.");
             return false;
         }
-        return ! allShips.containsKey(name);
+        return !allShips.containsKey(name);
     }
-    public void registerShip(Ship ship){
-        if(allShips.containsKey(ship.getName())){
+
+    public void registerShip(Ship ship) {
+        if (allShips.containsKey(ship.getName())) {
             Warnings.makeStrongWarning(toString(), "allShips already contains a ship with this name: " + ship.getName());
             return;
         }
         allShips.put(ship.getName(), ship);
     }
-    public void unregisterShip(String shipName){
-        if(allShips.containsKey(shipName))
+
+    public void unregisterShip(String shipName) {
+        if (allShips.containsKey(shipName))
             allShips.remove(shipName);
-        else{
-            Warnings.makeStrongWarning(toString()+".unregisterShip()", "Attempt to remove a ship whose name is unknown: " + shipName);
+        else {
+            Warnings.makeStrongWarning(toString() + ".unregisterShip()", "Attempt to remove a ship whose name is unknown: " + shipName);
         }
     }
 
@@ -123,18 +137,109 @@ public class Game {
         //this is a O(N) implementation. But there are just up to 10 teams in our game anyway...
         List<Team> result = getTeams().stream().filter(t -> t.getName().equals(teamName)).collect(Collectors.toList());
         int size = result.size();
-        if(size == 0){
+        if (size == 0) {
             Warnings.makeWarning(toString(), "No team with this name found: " + teamName);
-            return  null;
+            return null;
         }
-        if(size > 1) Warnings.panic(toString(), "More teams (" + size+ ") with the same name: " + teamName);
+        if (size > 1) Warnings.panic(toString(), "More teams (" + size + ") with the same name: " + teamName);
         return result.get(0);
     }
 
-    public static Game LoadFromModel(com.vztekoverflow.lospiratos.model.Game gameModel){
+    public static Game LoadFromModel(com.vztekoverflow.lospiratos.model.Game gameModel) {
         Game g = new Game(gameModel);
-        for(com.vztekoverflow.lospiratos.model.Team teamModel : gameModel.getTeams()){
+        for (com.vztekoverflow.lospiratos.model.Team teamModel : gameModel.getTeams()) {
             g.addNewTeamFromModel(teamModel);
+        }
+        return g;
+    }
+
+    private static String[] teamNames = {
+            "Pirrraten mit nem Schwert",
+            "You are a pirate",
+            "Los Piratos",
+            "Piráti na Vltavě",
+            "пираты z Ruska",
+            "Greek πειρατές",
+            "海賊 z Japonska",
+    };
+    private static String[] captainNames = {
+            "Jack",
+            "Jack Sparrow",
+            "Jack Black",
+            "Jack Daniels",
+            "Jack Reacher",
+            "Jack Kerouac",
+            "Jack Kennedy",
+            "Jack O'Neill",
+            "Jack Crawford",
+            "Jack London",
+            "Jack Nicholson",
+            "Handsome Jack",
+            "Jack de la Casa",
+            "Jim&Jack",
+            "Wild Jack",
+            "Jumpin' Jack",
+            "Jaccuzi",
+            "Jackitas",
+            "Jack and Jill",
+            "Big Jack",
+            "Hit the Road Jack",
+    };
+    private static Class<?>[] shipTypes = {
+            Schooner.class,
+            Brig.class,
+            Frigate.class,
+            Galleon.class
+    };
+    private static Class<?>[] shipEnhancements = {
+            CannonUpgrade.class,
+            ChainShot.class,
+            Ram.class,
+            Mortar.class,
+            HeavyShot.class,
+            HullUpgrade.class,
+    };
+
+
+    public static Game CreateNewMockGame() {
+        final int teamCount = 6; //beter do not make bigger than 6
+        int captainIdx =0;
+        Game g = new Game();
+        for (int i = 1; i <= teamCount; i++) {
+            String name = "(" + i + ") " + teamNames[i];
+            Color c = Color.color((255 - (i-1) * 49)/255d, (255 - (i-1) * 49)/255d, ((i-1) * 49d)/255d);
+            Team team = g.createAndAddNewTeam(name, c);
+            team.getOwnedResource().setMoney(5000 + i);
+            team.getOwnedResource().setCloth(10 * i);
+            team.getOwnedResource().setMetal(20 * i);
+            team.getOwnedResource().setRum(30 * i);
+            team.getOwnedResource().setWood(40 * i);
+            //Create ships:
+            for (int j = 0; j < i; j++) {
+                name = "Tým" + i + "_Loď" + j;
+                String captain = captainNames[captainIdx++];
+                Class<ShipType> type = (Class<ShipType>) shipTypes[j % 4];
+                Ship s = team.createAndAddNewShip(type,name, captain);
+                s.getStorage().addMoney(500 *i+10*j);
+                s.getStorage().addCloth(10 * i+j);
+                s.getStorage().addMetal(20 * i+j);
+                s.getStorage().addRum(30 * i+j);
+                s.getStorage().addWood(40 * i+j);
+                if(i!=3) //random value
+                    s.addToCurrentHP(-5*j);
+
+                for (int k = 0; k < j; k++) {
+                    Class<ShipEnhancement> enh = (Class<ShipEnhancement>) shipEnhancements[k];
+                    s.addNewEnhancement(enh);
+                }
+                if((i == 3 && j ==2) || (i == 5 && j ==4)) { //random values
+                    s.destroyShipAndEnhancements();
+                }
+                if(i==2){ //random value
+                    s.destroyShipAndEnhancements();
+                    s.repairShip();
+                }
+            }
         }
         return g;
     }
